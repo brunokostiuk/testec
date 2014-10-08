6cbf05b2ff22506ea734d133521f997c19d2ad96
Bruno Kostiuk <brunokostiuk@hotmail.com>
2014-10-08 15:29:17 -0300

    comit
    teste

---
+++ teste.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 1;
+    int num_temp;
+    int num_next = 1;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cout << num_next << "  ";
+        num_next = num1 + num2;
+        num1 = num2;
+        num_temp = num2;
+        num2 = num_next - num1;
+        num1 = num_temp;
+    }
+    return 0;
+}
